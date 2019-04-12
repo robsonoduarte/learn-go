02f3b41191651f892364d9450188d2424d264b25
robsonoduarte <robson.o.d@gmail.com>
2019-04-11 22:16:47 -0300

    aula sobre comandos do go

---
+++ curso-golang/go-fundamentos/commandos/comandos.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// commandos usado nessa aula
+// go env
+// go vet
+// godoc -http=:6060
+// go buil
+// go run
+// go version
+func main() {
+	fmt.Printf("Outro programa em %s", "GO")
+}
