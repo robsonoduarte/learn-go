415b09d8683a95c3093607426b99d5c685a12882
Robson Duarte <robson.o.d@gmail.com>
2020-11-18 20:02:06 -0300

    :rocket:

--- go-curse/type-system/custom-types/custom.go
+++ go-curse/type-system/custom-types/custom.go
@@ -1,11 +1,30 @@
-package custom_types
+package main
 
+import "fmt"
 
 type score float64
 
-func (s score ) inside(start, end float64) bool {
+func (s score) inside(start, end float64) bool {
 	return float64(s) >= start && float64(s) <= end
 }
 
+func x(s score) string {
+	switch {
+	case s.inside(9.0, 10.0):
+		return "A"
+	case s.inside(7.0, 8.99):
+		return "B"
+	case s.inside(5.0, 7.99):
+		return "C"
+	case s.inside(3.0, 4.99):
+		return "D"
+	default:
+		return "E"
+	}
+}
 
-
+func main() {
+	fmt.Println(x(9))
+	fmt.Println(x(6.9))
+	fmt.Println(x(2.1))
+}
